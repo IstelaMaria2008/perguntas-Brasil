e4347c3c623fd140baaa468b73827c43bc80d47c
IstelaMaria2008 <00001127011649sp@al.educacao.sp.gov.br>
2024-11-25 08:50:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'SURGIMENTO',
+    'quantos anos tem o Brasil?',
+    'O brasil tem 520 anos,desde que foi descoberto'
+)
+
+criaCartao(
+    'Parcerias',
+    'Qual o principal parceiro economico do Brasil?',
+    'A China'
+)
+
+criaCartao(
+    'Fashion Models',
+    'Qual as 2 modelos Brasileiras que sao conhecidas mundialmente?',
+    'Gisele Bundchen e Adriana Lima'
+)
+
+criaCartao(
+    'Festa e jogos',
+    'O brasil é famoso por ser um pais do?',
+    'Do carnaval e do fultbol'
+)
